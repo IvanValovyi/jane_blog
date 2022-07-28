052da2af2f0b99b76050a5d224ea911df786b887
IvanValovyi <pan.ivan.17.0@gmail.com>
2022-07-28 17:21:16 +0300

    Init

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+
+export function subscribe_animation(){let subscribe_menu=document.querySelector('.subscribe_menu')
+let open_subscribe_btns=document.querySelectorAll('button.subscribe.open')
+let close_subscribe_btns=subscribe_menu.querySelectorAll('.close')
+open_subscribe_btns.forEach(btn=>{btn.addEventListener('click',()=>{subscribe_menu.classList.remove('display_none')})})
+close_subscribe_btns.forEach(btn=>{btn.addEventListener('click',()=>{subscribe_menu.classList.add('display_none')})})}
